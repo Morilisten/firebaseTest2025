ef07487eed3ff8fef506a3c701cf30a16454381b
morilisten <yangshun1997@gmail.com>
2025-08-09 11:03:34 +0800

    feat: 新增useChat文件 docs: 已連接上firebase資料庫 API已設定完成

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@ import { collection, getDocs } from 'firebase/firestore'
 import { db } from '@/lib/firebase'
 const testFirestore = async () => {
   try {
-    const querySnapshot = await getDocs(collection(db, 'test'))
-    debugger
+    const querySnapshot = await getDocs(collection(db, 'messages'))
     querySnapshot.forEach(doc => {
       console.log(`${doc.id} =>`, doc.data())
     })
